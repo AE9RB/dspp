9a6c5b83d0e87ccca655fe92202e8b6e0ba3bba6
David Turnbull <dturnbull@gmail.com>
2014-06-25 21:54:20 -0700

    Added compile time constants for FFT

--- include/fft.hpp
+++ include/fft.hpp
@@ -19,35 +19,65 @@
 
 #include <complex>
 #include <array>
+#include <vector>
 #include <cmath>
 #include "util.hpp"
 
 namespace dspp {
     namespace FFT {
 
-        /// Recursive template for butterfly mixing.
-        template<typename T, size_t N, int D>
-        class Radix4 {
+        // Twiddle factors
+        template<typename T, int D, size_t N>
+        struct Twiddle {
             static const std::array<std::complex<T>, N/4> t1;
             static const std::array<std::complex<T>, N/4> t2;
             static const std::array<std::complex<T>, N/4> t3;
+        };
+        template<typename T>
+        static std::vector<std::complex<T>> twiddles(double a, int d, size_t n) {
+            std::vector<std::complex<T>> twids(n/4);
+            double theta = two_pi<double>()*d/n;
+            for (size_t i=0; i < n/4; ++i) {
+                double phi = theta * a * i;
+                twids[i] = std::complex<T>(cos(phi), sin(phi));
+            }
+            return twids;
+        }
+        template<typename T, int D, size_t N>
+        const std::array<std::complex<T>, N/4> Twiddle<T, D, N>::t1(
+            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddles<T>(1,D,N).data())
+        );
+        template<typename T, int D, size_t N>
+        const std::array<std::complex<T>, N/4> Twiddle<T, D, N>::t2(
+            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddles<T>(2,D,N).data())
+        );
+        template<typename T, int D, size_t N>
+        const std::array<std::complex<T>, N/4> Twiddle<T, D, N>::t3(
+            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddles<T>(3,D,N).data())
+        );
+
+        // Recursive template for butterfly mixing.
+        template<typename T, int D, size_t N>
+        class Butterfly {
+            static const Twiddle<T, D, N> twiddle;
             static const size_t N4 = N/4;
-            Radix4<T, N4, D> next;
-            /// Simplified multiplication for direction product.
-            static inline std::complex<T> direction(const std::complex<T>& z)
+            static Butterfly<T, D, N4> next;
+            // Simplified multiplication for direction product.
+            static std::complex<T> direction(const std::complex<T>& z)
             {
                 if (D>0) return std::complex<T>(-z.imag(), z.real());
                 else return std::complex<T>(z.imag(), -z.real());
             }
         public:
-            void operator()(std::complex<T>* data) {
+            // Radix-4 mixer
+            static void mix(std::complex<T>* data) {
                 size_t i1 = N4;
-                size_t i2 = N4 + N4;
-                size_t i3 = i2 + N4;
-                next(data);
-                next(data+i1);
-                next(data+i2);
-                next(data+i3);
+                size_t i2 = N4 * 2;
+                size_t i3 = N4 * 3;
+                next.mix(data);
+                next.mix(data+i1);
+                next.mix(data+i2);
+                next.mix(data+i3);
                 // Index 0 twiddles are always (1+0i).
                 std::complex<T> a0 = data[0];
                 std::complex<T> a2 = data[i1];
@@ -65,9 +95,9 @@ namespace dspp {
                     i2 = i1 + N4;
                     i3 = i2 + N4;
                     a0 = data[i0];
-                    a2 = mul(data[i1], t2[i0]);
-                    a1 = mul(data[i2], t1[i0]);
-                    a3 = mul(data[i3], t3[i0]);
+                    a2 = mul(data[i1], twiddle.t2[i0]);
+                    a1 = mul(data[i2], twiddle.t1[i0]);
+                    a3 = mul(data[i3], twiddle.t3[i0]);
                     b0 = a1 + a3;
                     b1 = direction(a1-a3);
                     data[i0] = a0 + a2 + b0;
@@ -78,37 +108,12 @@ namespace dspp {
             }
         };
 
-        /// Computes twiddle factors
-        template<typename T>
-        static std::vector<std::complex<T>> twiddle4(double a, size_t n, int d) {
-            std::vector<std::complex<T>> twids(n/4);
-            double theta = two_pi<double>()*d/n;
-            for (size_t i=0; i < n/4; ++i) {
-                double phi = theta * a * i;
-                twids[i] = std::complex<T>(cos(phi), sin(phi));
-            }
-            return twids;
-        }
-
-        // Storage and initialization for twiddle factors
-        template<typename T, size_t N, int D>
-        const std::array<std::complex<T>, N/4> Radix4<T, N, D>::t1(
-            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddle4<T>(1,N,D).data())
-        );
-        template<typename T, size_t N, int D>
-        const std::array<std::complex<T>, N/4> Radix4<T, N, D>::t2(
-            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddle4<T>(2,N,D).data())
-        );
-        template<typename T, size_t N, int D>
-        const std::array<std::complex<T>, N/4> Radix4<T, N, D>::t3(
-            *reinterpret_cast<std::array<std::complex<T>, N/4>*>(twiddle4<T>(3,N,D).data())
-        );
-
-        /// Terminates template recursion when not power of 4.
+        // Terminates template recursion when not power of 4.
         template<typename T, int D>
-        class Radix4<T, 2, D> {
+        class Butterfly<T, D, 2> {
         public:
-            inline void operator()(std::complex<T>* data) {
+            // Radix-2 mixer
+            static void mix(std::complex<T>* data) {
                 std::complex<T> a0 = data[0];
                 std::complex<T> a1 = data[1];
                 data[0] = a0 + a1;
@@ -116,58 +121,62 @@ namespace dspp {
             }
         };
 
-        /// Terminates template recursion for powers of 4.
+        // Terminates template recursion for powers of 4.
         template<typename T, int D>
-        class Radix4<T, 1, D> {
+        class Butterfly<T, D, 1> {
         public:
-            inline void operator()(std::complex<T>* data) {
+            static void mix(std::complex<T>* data) {
                 // Do nothing.
             }
         };
 
-        // Computes the bit reversal pattern for size N
-        template<size_t N>
+        // Bit reversal pattern
+        template<size_t N, bool ispow4>
         struct BitReverse {
-            static std::vector<size_t> pattern;
-            static bool ispow4;
-            static std::vector<size_t> calc() {
-                std::vector<size_t> l;
-                l.push_back(0);
-                size_t m = 1;
-                size_t n = N;
-                while ((m << 2) < n) {
-                    n >>= 1;
-                    for (size_t j = 0; j < m; j++) {
-                        l.push_back(l[j] + n);
-                    }
-                    m <<= 1;
-                }
-                ispow4 = (m << 2) == n;
-                l.shrink_to_fit();
-                return l;
-            }
+            static const std::array<size_t, N> pattern;
         };
+        static std::vector<size_t> bitpattern(size_t n, bool ispow4) {
+            std::vector<size_t> l;
+            n = (n*n) << 1;
+            if (ispow4) n <<= 1;
+            l.push_back(0);
+            size_t m = 1;
+            while ((m << 2) < n) {
+                n >>= 1;
+                for (size_t j = 0; j < m; j++) {
+                    l.push_back(l[j] + n);
+                }
+                m <<= 1;
+            }
+            return l;
+        }
+        template<size_t N, bool ispow4>
+        const std::array<size_t, N> BitReverse<N, ispow4>::pattern(
+            *reinterpret_cast<std::array<size_t, N>*>(bitpattern(N,ispow4).data())
+        );
 
-        template<size_t N>
-        std::vector<size_t> BitReverse<N>::pattern = BitReverse<N>::calc();
-        template<size_t N>
-        bool BitReverse<N>::ispow4;
-
-        /// Fast Fourier Transform.
+        // Start of Fourier Transforms.
         template<typename T, size_t N>
-        class FFT : public BitReverse<N> {
+        class Transform {
             static_assert((N > 1) & !(N & (N - 1)), "Array size must be a power of two.");
-            using BitReverse<N>::pattern;
-            using BitReverse<N>::ispow4;
-
+            static constexpr bool ispow4_impl(size_t n, size_t m ) {
+                return ((m << 2) < n) ? ispow4_impl(n >> 1, m << 1) : (m << 2) == n;
+            }
+            static constexpr size_t ispow4 = ispow4_impl(N,1);
+            static constexpr size_t pattern_size_impl(size_t n, size_t m) {
+                return ((m << 2) < n) ? pattern_size_impl(n >> 1, m << 1) : m;
+            }
+            static constexpr size_t pattern_size = pattern_size_impl(N,1);
+            static const BitReverse<pattern_size, ispow4> bit;
             static void reindex(std::array<std::complex<T>, N> &data) {
-                size_t j1, k1, m = pattern.size();
+                size_t j1, k1;
+                constexpr size_t m = bit.pattern.size();
+                constexpr size_t m2 = 2 * m;
                 if (ispow4) {
-                    size_t m2 = 2 * m;
                     for (size_t k = 0; k < m; k++) {
                         for (size_t j = 0; j < k; j++) {
-                            j1 = j + pattern[k];
-                            k1 = k + pattern[j];
+                            j1 = j + bit.pattern[k];
+                            k1 = k + bit.pattern[j];
                             std::swap(data[j1], data[k1]);
                             j1 += m;
                             k1 += m2;
@@ -179,15 +188,15 @@ namespace dspp {
                             k1 += m2;
                             std::swap(data[j1], data[k1]);
                         }
-                        j1 = k + m + pattern[k];
+                        j1 = k + m + bit.pattern[k];
                         k1 = j1 + m;
                         std::swap(data[j1], data[k1]);
                     }
                 } else {
                     for (size_t k = 1; k < m; k++) {
                         for (size_t j = 0; j < k; j++) {
-                            j1 = j + pattern[k];
-                            k1 = k + pattern[j];
+                            j1 = j + bit.pattern[k];
+                            k1 = k + bit.pattern[j];
                             std::swap(data[j1], data[k1]);
                             j1 += m;
                             k1 += m;
@@ -196,15 +205,15 @@ namespace dspp {
                     }
                 }
             }
-
             static void reindex(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) {
-                size_t j1, k1, m = pattern.size();
+                size_t j1, k1;
+                constexpr size_t m = bit.pattern.size();
+                constexpr size_t m2 = 2 * m;
                 if (ispow4) {
-                    size_t m2 = 2 * m;
                     for (size_t k = 0; k < m; k++) {
                         for (size_t j = 0; j < k; j++) {
-                            j1 = j + pattern[k];
-                            k1 = k + pattern[j];
+                            j1 = j + bit.pattern[k];
+                            k1 = k + bit.pattern[j];
                             out[j1] = in[k1];
                             out[k1] = in[j1];
                             j1 += m;
@@ -220,7 +229,7 @@ namespace dspp {
                             out[j1] = in[k1];
                             out[k1] = in[j1];
                         }
-                        k1 = k + pattern[k];
+                        k1 = k + bit.pattern[k];
                         out[k1] = in[k1];
                         j1 = k1 + m;
                         k1 = j1 + m;
@@ -234,8 +243,8 @@ namespace dspp {
                     out[m] = in[m];
                     for (size_t k = 1; k < m; k++) {
                         for (size_t j = 0; j < k; j++) {
-                            j1 = j + pattern[k];
-                            k1 = k + pattern[j];
+                            j1 = j + bit.pattern[k];
+                            k1 = k + bit.pattern[j];
                             out[j1] = in[k1];
                             out[k1] = in[j1];
                             j1 += m;
@@ -243,100 +252,82 @@ namespace dspp {
                             out[j1] = in[k1];
                             out[k1] = in[j1];
                         }
-                        k1 = k + pattern[k];
+                        k1 = k + bit.pattern[k];
                         out[k1] = in[k1];
                         out[k1+m] = in[k1+m];
                     }
                 }
             }
-
-            const std::array<std::complex<T>, N> *in;
-            std::array<std::complex<T>, N> *out;
         public:
-            FFT(std::array<std::complex<T>, N> &data) :
-                in(nullptr), out(&data) {
-            }
-            FFT(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) :
-                in(&in), out(&out) {
-            }
-            virtual void dft() {
-                if (in==nullptr) dft(*out);
-                else dft(*in, *out);
-            }
-            virtual void idft() {
-                if (in==nullptr) idft(*out);
-                else idft(*in, *out);
-            }
             static void dft(std::array<std::complex<T>, N> &data) {
-                Radix4<T, N, -1> butterfly;
                 reindex(data);
-                butterfly(&data[0]);
+                Butterfly<T, -1, N>::mix(&data[0]);
             }
             static void idft(std::array<std::complex<T>, N> &data) {
-                Radix4<T, N, 1> butterfly;
                 reindex(data);
-                butterfly(&data[0]);
+                Butterfly<T, 1, N>::mix(&data[0]);
             }
             static void dft(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) {
-                Radix4<T, N, -1> butterfly;
                 reindex(in, out);
-                butterfly(&out[0]);
+                Butterfly<T, -1, N>::mix(&out[0]);
             }
             static void idft(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) {
-                Radix4<T, N, 1> butterfly;
                 reindex(in, out);
-                butterfly(&out[0]);
+                Butterfly<T, 1, N>::mix(&out[0]);
             }
         };
 
         /// Discrete Fourier transform.
         template<typename T, size_t N>
         inline void dft(std::array<std::complex<T>, N> &data) {
-            FFT<T, N>::dft(data);
+            Transform<T, N>::dft(data);
         }
 
         /// Discrete Fourier transform.
         template<typename T, size_t N>
         inline void dft(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) {
-            FFT<T, N>::dft(in, out);
+            Transform<T, N>::dft(in, out);
         }
 
         /// Discrete Fourier transform.
         template<typename T, size_t N>
         inline void dft(std::complex<T> (&data)[N]) {
-            FFT<T, N>::dft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&data));
+            Transform<T, N>::dft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&data));
         }
 
         /// Discrete Fourier transform.
         template<typename T, size_t N>
         inline void dft(const std::complex<T> (&in)[N], std::complex<T> (&out)[N]) {
-            FFT<T, N>::dft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&in), *reinterpret_cast<std::array<std::complex<T>, N>*>(&out));
+            Transform<T, N>::dft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&in),
+                                 *reinterpret_cast<std::array<std::complex<T>, N>*>(&out));
         }
 
         /// Inverse discrete Fourier transform.
         template<typename T, size_t N>
         inline void idft(std::array<std::complex<T>, N> &data) {
-            FFT<T, N>::idft(data);
+            Transform<T, N>::idft(data);
         }
 
         /// Inverse discrete Fourier transform.
         template<typename T, size_t N>
         inline void idft(const std::array<std::complex<T>, N> &in, std::array<std::complex<T>, N> &out) {
-            FFT<T, N>::idft(in, out);
+            Transform<T, N>::idft(in, out);
         }
 
         /// Inverse discrete Fourier transform.
         template<typename T, size_t N>
         inline void idft(std::complex<T> (&data)[N]) {
-            FFT<T, N>::idft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&data));
+            Transform<T, N>::idft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&data));
         }
 
         /// Inverse discrete Fourier transform.
         template<typename T, size_t N>
         inline void idft(const std::complex<T> (&in)[N], std::complex<T> (&out)[N]) {
-            FFT<T, N>::idft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&in), *reinterpret_cast<std::array<std::complex<T>, N>*>(&out));
+            Transform<T, N>::idft(*reinterpret_cast<std::array<std::complex<T>, N>*>(&in),
+                                  *reinterpret_cast<std::array<std::complex<T>, N>*>(&out));
         }
 
     }
 }
 #endif
+
